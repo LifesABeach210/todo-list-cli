2118aa5426f389bf490289aa1dcb5986c5e4b535
Dan <danielrothwell622@gmail.com>
2022-02-23 14:06:46 -0500

    hello

--- List.js
+++ List.js
@@ -6,18 +6,18 @@ while (FirstPrompt!==prompt("exit")) {
 let FirstPrompt = prompt(">Please Press One [1] To Add Items To Your To Do List Or Press two [2] To View Items Already Added"+str)
     
 str= str+=strZ
-
 }*/
 
 
     
-let FirstPrompt = prompt(">Please press [1] to add items to your to do list")
-if (FirstPrompt = "1") {
+let FirstPrompt = prompt(">Please press [1] to add items to your to do list ");
+if (FirstPrompt === "1"); {
 let List = [];
 let ListC =[];
-const complete = [true,false];
+const complete = [":complete"];
+const incomplete =["incomplete"];
+let count = 0;
 while (FirstPrompt!=="exit") {
- 
  
  
 if (FirstPrompt==="1") {
@@ -27,38 +27,37 @@ if (FirstPrompt==="1") {
     const isComplete = List[true,false];
     for (let i = 0; i < List.length; i++) {
  
-    console.log(List[i] + complete);
+    console.log(incomplete, List[i]);
 
 
 
 }
 
 }else if (FirstPrompt === "2"){
-Task = prompt("if you wish to complete the first tast you entered press 2 otherwise press 1:>  ")
-let ListC = List.shift(0)
-for (let i = 0; i < ListC.length; i++) {
+Task = prompt("if you wish to complete the first tast you entered press 2 otherwise press 1:>  ");
+ ListC.shift(List);
+for (let i = 0; i < ListC.length; i++); {
     
-    console.log("here are you completed items " +ListC);
-    
-}
-
+    console.log("here are you completed items "+ListC);
     
 }
 
     
+}
+
+    
 
 
 
-FirstPrompt =prompt("please press 1 to add items to list or press 2 to complete listed items or exit to leave:> ")
+FirstPrompt =prompt("please press 1 to add items to list or press 2 to complete listed items or exit to leave:> ");
 }
    
 
 
 
-
-
-
 }
+
+
 
 
 
